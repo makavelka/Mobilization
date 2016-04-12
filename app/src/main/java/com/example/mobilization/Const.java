34833a7e21e6dcd05509ce81a9f7588a99746b4c
makavelka <vovchilnik@yandex.ru>
2016-04-13 00:45:09 +0300

    Common class for const values

---
+++ app/src/main/java/com/example/mobilization/Const.java
@@ -0,0 +1,11 @@
+package com.example.mobilization;
+
+public interface Const {
+
+        String UI_THREAD = "UI_THREAD";
+        String IO_THREAD = "IO_THREAD";
+
+        String BASE_URL = "https://cache-stav01.cdn.yandex.net/";
+
+
+}
