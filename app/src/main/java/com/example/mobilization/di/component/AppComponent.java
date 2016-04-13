1ee222de0bdcf3fc7f7819aecd5bab39451f5df9
makavelka <vovchilnik@yandex.ru>
2016-04-14 01:37:12 +0300

    Add inject methods and modules

--- app/src/main/java/com/example/mobilization/di/component/AppComponent.java
+++ app/src/main/java/com/example/mobilization/di/component/AppComponent.java
@@ -1,11 +1,16 @@
 package com.example.mobilization.di.component;
 
-import com.example.mobilization.di.modules.ApiModule;
+import com.example.mobilization.Utils;
+import com.example.mobilization.di.modules.AppModule;
 import com.example.mobilization.di.modules.ModelModule;
 import com.example.mobilization.di.modules.PresenterModule;
+import com.example.mobilization.di.modules.UtilsModule;
 import com.example.mobilization.di.modules.ViewModule;
 import com.example.mobilization.model.ModelImpl;
+import com.example.mobilization.model.api.ApiService;
+import com.example.mobilization.model.api.ResponseCacheInterceptor;
 import com.example.mobilization.presenter.ArtistListPresenter;
+import com.example.mobilization.presenter.BasePresenter;
 import com.example.mobilization.view.activity.MainActivity;
 
 import javax.inject.Singleton;
@@ -13,9 +18,13 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {ApiModule.class, PresenterModule.class, ViewModule.class, ModelModule.class})
+@Component(modules = {PresenterModule.class, ViewModule.class, ModelModule.class, AppModule.class, UtilsModule.class})
 public interface AppComponent {
     void inject(ModelImpl model);
     void inject(ArtistListPresenter presenter);
     void inject(MainActivity activity);
+    void inject(BasePresenter presenter);
+    void inject(Utils utils);
+    void inject(ResponseCacheInterceptor interceptor);
+    void inject(ApiService api);
 }
